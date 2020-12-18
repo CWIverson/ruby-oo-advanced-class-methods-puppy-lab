f9ecae26d324ccd299685055ae6d6da26b8da78d
christian iverson <chris.w.iverson@gmail.com>
2020-12-18 12:43:37 -0800

    commit tree d7d8062628a469d86c449934ba1ba73ef534eb57

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+require "pry"
+class Dog
+   @@all=[]
+    def initialize(name)
+        @name = name
+        
+        self.save
+        
+    end
+    def name
+        @name
+    end
+    def self.all
+        @@all
+    end
+    def self.clear_all
+        @@all.clear
+    end
+    def self.print_all
+        @@all.each {|e|
+            puts e.name
+        }
+    end
+    def save
+        @@all<<self
+        
+
+    end
+end
